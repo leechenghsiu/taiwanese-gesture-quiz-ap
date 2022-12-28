0e6576d7c0b1656a6d62456013d3cb709724ed2f
Matthew <matthew6303@gmail.com>
2022-12-28 11:03:51 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
